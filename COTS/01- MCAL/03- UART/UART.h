225d8611301334a4c1344e47593abd9b66068041
xomayax <87386122+xomayax@users.noreply.github.com>
2023-04-26 02:24:47 +0200

    Update UART.h

--- COTS/01- MCAL/03- UART/UART.h
+++ COTS/01- MCAL/03- UART/UART.h
@@ -2,14 +2,14 @@
 #define UART_H
 
 
-#include "../LIB/STD_TYPES.h"
-#include "../LIB/ERROR_STATE.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/ERROR_STATE.h"
 
 void UART_Init(void);
-void UART_sendByte(const uint8 data);
-uint8 UART_recieveByte(void);
-void UART_sendString(const uint *Str);
-void UART_receiveString(uint8 *Str);
+void UART_sendByte(const u8 data);
+u8 UART_recieveByte(void);
+void UART_sendString(const u8 *Str);
+void UART_receiveString(u8 *Str);
 
 
 
