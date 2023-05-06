bfd29782aabb253f5f8eb558057817a72d871322
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2023-05-06 15:42:43 +0300

    add cmake build

---
+++ Build/01-MCAL/01-DIO/CMakeFiles/dio.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mohamed/EmbeddedProj/Embedded-GPS/COTS/01-MCAL/01-DIO/DIO_program.c" "01-MCAL/01-DIO/CMakeFiles/dio.dir/DIO_program.c.o" "gcc" "01-MCAL/01-DIO/CMakeFiles/dio.dir/DIO_program.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/01-MCAL/02-STK/CMakeFiles/stk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stk.dir/STK_program.c.o"
+  "CMakeFiles/stk.dir/STK_program.c.o.d"
+  "libstk.a"
+  "libstk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
