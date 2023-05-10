45727751d9e1db2bc574a803baf20430a07577ac
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-05-10 19:21:15 +0300

    NO COMMIT

--- COTS/02-HAL/03-GPS/GPS_private.h
+++ COTS/02-HAL/03-GPS/GPS_private.h
@@ -5,11 +5,11 @@
  *      Author: somay
  */
 
-#ifndef GPS_PRIVATE_H_
-#define GPS_PRIVATE_H_
+#ifndef __GPS_PRIVATE_H__
+#define __GPS_PRIVATE_H__
 
 #define M_PI            3.1415926
 #define Earth_Radius    6371000
 
 
-#endif /* GPS_PRIVATE_H_ */
+#endif /* __GPS_PRIVATE_H__ */
