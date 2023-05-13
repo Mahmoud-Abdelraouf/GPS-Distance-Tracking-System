b13f238e9263cd65a3dbb55c30b8c11c0c02d9a4
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-05-13 13:24:02 +0300

    ...

--- GPS_Distance_Tracking_System/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/ccs/Debug/driverlib.lib"
-
